49f5e55cb8aa4d828294b8dae6e0111a9bbee1f1
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-11 22:13:31 +0300

    deploying: to heroku

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 def mail_message(subject,template,to,**kwargs):
     subject_pref = 'pitch'
-    sender_email = 'wangu.mwangi@student.moringaschool.com'
+    sender_email = 'elijahwangu91@gmail.com'
 
 
     email = Message(subject, sender=sender_email, recipients=[to])
--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
     SECRET_KEY='5678'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://eayqbjrfoytgud:66cf71c74cce1297c26bf44bab6eb1f91d812be9fb23b98f54eeb39e2f78a7df@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d8kju1nsufb22n'
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:enter@localhost/visual'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SUBJECT_PREFIX = 'pitch'
-    SENDER_EMAIL = 'wangu.mwangi@student.moringaschool.com'
+    SENDER_EMAIL = 'elijahwangu91@gmail.com'
 
     
    
@@ -30,7 +30,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI= 'postgresql://eayqbjrfoytgud:66cf71c74cce1297c26bf44bab6eb1f91d812be9fb23b98f54eeb39e2f78a7df@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d8kju1nsufb22n'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:enter@localhost/visual'
     # uri = os.getenv('DATABASE_URL')
     # if uri and uri.startswith('postgres://'):
     #     uri = uri.replace('postgres://', 'postgresql://', 1)
@@ -39,10 +39,11 @@ class ProdConfig(Config):
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://eayqbjrfoytgud:66cf71c74cce1297c26bf44bab6eb1f91d812be9fb23b98f54eeb39e2f78a7df@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d8kju1nsufb22n'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:enter@localhost/visual'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://eayqbjrfoytgud:66cf71c74cce1297c26bf44bab6eb1f91d812be9fb23b98f54eeb39e2f78a7df@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d8kju1nsufb22n'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://eayqbjrfoytgud:66cf71c74cce1297c26bf44bab6eb1f91d812be9fb23b98f54eeb39e2f78a7df@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d8kju1nsufb22n'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:enter@localhost/visual'
     # DEBUG = True
 
 config_options = {
